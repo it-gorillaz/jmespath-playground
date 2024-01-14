14371b08f9ec42bca52c19a511105c0e7ee4df55
tom-melo <neilton.demelo@emnify.com>
2024-01-14 18:08:55 +0000

    add test folder

---
+++ src/test/java/com/itgorillaz/jmespathplayground/JmespathPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itgorillaz.jmespathplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmespathPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
